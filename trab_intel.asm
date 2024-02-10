76719a4acb5e28471175413345747de93b46b741
Gabriel Ricci Pazinatto <gabrielrpazinatto@gmail.com>
2024-02-10 14:22:47 -0300

    partially implemented input checking

--- trab_intel.asm
+++ trab_intel.asm
@@ -1,4 +1,4 @@
- .MODEL small
+.MODEL small
 
 .STACK 1024
 
@@ -18,148 +18,89 @@
 	in_buffer				DB 100 DUP(?)
 	input_file_name			DB 100 DUP(end_of_string)
 	output_file_name		DB 100 DUP(end_of_string)
-	tension_atoi            DB 4 DUP(end_of_string)
+	voltage_atoi            DB 10 DUP(end_of_string)
+	voltage					Dw 0
 
-	default_input_file_name DB "a.in"
-	default_output_file_name DB "a.out"
+	default_input_file_name DB "a.in",end_of_string
+	default_output_file_name DB "a.out",end_of_string
+
+	msg_error_open_file		DB "Arquivo nao pode ser aberto.", end_of_string
+	msg_error_invalid_voltage DB "Valor de tensao incorreta.", end_of_string
+
 
 .CODE
 
-
-
-printf PROC NEAR
-	mov ah, 09h
-	int 21h
-	ret
-	printf ENDP
-
+;di <- destiny
+;si <- source
 
 strcpy_s PROC NEAR
+	mov bl, [si]
+	cmp bl, space
+	je ret_strcpy
+	cmp bl, end_of_string
+	je ret_strcpy
 
-	strcpy_loop:
-		mov al, [si]
-		cmp al, end_of_string
-		je	ret_strcpy
-		cmp al, space
-		je	ret_strcpy
-
-		mov [di], al
-		inc di
-		inc si
-		JMP strcpy_loop
+	mov [di], bl
+	inc si
+	inc di
+	jmp strcpy_s
 
 	ret_strcpy:
 		inc di
 		mov [di], end_of_string
 		ret
 		strcpy_s ENDP
-		
 
------------------------------------------------------------
-gets	proc	near
-	mov		ah,0ah						; L� uma linha do teclado
-	lea		dx, in_buffer
-	mov		byte ptr in_buffer, 96	; 2 caracteres no inicio e um eventual CR LF no final
-	int		21h
+;----------------------------------------------
+get_argv PROC NEAR
 
-	lea		si,in_buffer+2					; Copia do buffer de teclado para o FileName
-	pop		di
-	mov		cl,in_buffer+1
-	mov		ch,0
-	mov		ax,ds						; Ajusta ES=DS para poder usar o MOVSB
-	mov		es,ax
-	rep 	movsb
+	push ds ; Salva as informações de segmentos
+	push es
+	mov ax,ds ; Troca DS com ES para poder usa o REP MOVSB
+	mov bx,es
+	mov ds,bx
+	mov es,ax
+	mov si,80h ; Obtém o tamanho do string da linha de comando e coloca em CX
+	mov ch,0
+	mov cl,[si]
+	mov ax,cx ; Salva o tamanho do string em AX, para uso futuro
+	mov si,81h ; Inicializa o ponteiro de origem
+	lea di, in_buffer ; Inicializa o ponteiro de destino
+	rep movsb
+	pop es ; retorna as informações dos registradores de segmentos
+	pop ds
 
-	mov		byte ptr es:[di],0			; Coloca marca de fim de string
 	ret
-gets	endp
 
+	get_argv ENDP
 
+;----------------------------------------------
+	printf_s	proc	near
 
-;---------------------------get_argv-----------------------
-;in_buffer <- argv
+	;	While (*s!='\0') {
+		mov		dl,[bx]
+		cmp		dl,0
+		je		ps_1
 
-get_argv_pointer PROC NEAR
-	mov AH, 0AH
-	lea dx, in_buffer
-	int 21h
-	
-	RET
-	get_argv_pointer ENDP
-;---------------------------get_file_name-----------------------
-;input_file_name <- nome do arquivo de input 
+	;		putchar(*s)
+		push	bx
+		mov		ah,2
+		int		21H
+		pop		bx
 
-get_file_name PROC NEAR
-	lea	si,	in_buffer
-
-	get_file_name_loop:
-		mov	al,	[si]
-		inc si
-		mov ah,	[si]
-
-		cmp al, end_of_string
-		JE	ret_get_file_name
-
-		cmp	ah, '-'
-		JE	possible_argument
-
-		possible_argument:
-			cmp ah,	'i'
-			JE input_found
-			JMP	get_file_name_loop
-
-		input_found:
-			inc si
-			inc si
-			mov	al,	[si]
-			cmp	al, '-'
-			JE	ret_get_file_name
-			cmp	al, end_of_string
-			JE	ret_get_file_name
+	;		++s;
+		inc		bx
 			
-			lea di, input_file_name
-
-			call strcpy_s
-
-		ret_get_file_name:
-			ret
-			get_file_name ENDP
+	;	}
+		jmp		printf_s
 			
+	ps_1:
 
-get_output_file_name PROC NEAR
-	lea si, in_buffer
-
-	output_file_name_loop:
-		mov ah, [si]
-
-		cmp ah, end_of_string
-		JE ret_get_output_name
-
-		cmp ah, '-'
-		JE possible_argument2
-
-		inc si
-		jmp output_file_name_loop
-
-		possible_argument2:
-			inc si
-			mov ah, [si]
-			cmp ah, 'o'
-
-			je output_name_found
-			jmp output_file_name_loop
-
-		output_name_found:
-			inc si
-			inc si
-			lea di, output_file_name
-			call strcpy_s
-	
-	ret_get_output_name:
 		ret
-		get_output_file_name ENDP
+		
+	printf_s	endp
 
-
+;----------------------------------------------
 get_tension PROC NEAR
 	lea si, in_buffer
 
@@ -185,7 +126,7 @@ get_tension PROC NEAR
 		voltage_found:
 			inc si
 			inc si
-			lea di, tension_atoi
+			lea di, voltage_atoi
 
 			call strcpy_s
 
@@ -193,31 +134,203 @@ get_tension PROC NEAR
 		ret
 		get_tension ENDP
 
+get_output_file_name PROC NEAR
+	lea si, in_buffer
 
+	output_file_name_loop:
+		mov al, [si]
+
+		cmp al, end_of_string
+		JE output_file_name_not_found
+
+		cmp al, '-'
+		JE possible_argument2
+
+		inc si
+		jmp output_file_name_loop
+
+		possible_argument2:
+			inc si
+			mov ah, [si]
+			cmp ah, 'o'
+
+			je output_name_found
+			jmp output_file_name_loop
+
+		output_file_name_not_found:
+			lea si, default_output_file_name
+			lea di, output_file_name
+
+			call strcpy_s
+
+			jmp ret_get_file_name
+
+		output_name_found:
+			inc si
+			inc si
+			lea di, output_file_name
+			call strcpy_s
+	
+	ret_get_output_name:
+		ret
+		get_output_file_name ENDP
+
+get_file_name PROC NEAR
+	lea	si,	in_buffer
+
+	get_file_name_loop:
+		mov	al,	[si]
+
+		cmp al, end_of_string
+		JE	input_file_name_not_found
+
+		cmp	al, '-'
+		JE	possible_argument
+
+		inc si
+		jmp get_file_name_loop
+
+		possible_argument:
+			inc si
+			mov al, [si]
+			cmp al,	'i'
+			
+			JE input_found
+			JMP	get_file_name_loop
+
+		input_file_name_not_found:
+			lea si, default_input_file_name
+			lea di, input_file_name
+
+			call strcpy_s
+
+			jmp ret_get_file_name
+
+		input_found:
+			inc si
+			inc si
+
+			lea di, input_file_name
+
+			call strcpy_s
+
+		ret_get_file_name:
+			ret
+	get_file_name ENDP
+
+
+open_file PROC NEAR
+	test al, al
+
+	mov al, 0
+	mov ah, 3dh
+	int 21H
+	mov bx, ax
+
+	jc perror_open_file
+	ret
+
+	perror_open_file:
+		lea bx, msg_error_open_file
+		call printf_s
+
+		mov ah, 4ch
+		mov al, 1
+		int 21h
+	
+	ret
+	open_file ENDP
+
+atoi	proc near
+		; A = 0;
+		mov		ax,0
+atoi_2:
+		; while (*S!='\0') {
+		cmp		byte ptr[bx], 0
+		jz		atoi_1
+		; 	A = 10 * A
+		mov		cx,10
+		mul		cx
+		; 	A = A + *S
+		mov		ch,0
+		mov		cl,[bx]
+		add		ax,cx
+		; 	A = A - '0'
+		sub		ax,'0'
+		; 	++S
+		inc		bx
+		;}
+		jmp		atoi_2
+atoi_1:
+		; return
+		ret
+atoi	endp
+
+check_voltage PROC NEAR
+	mov ax, voltage
+	cmp voltage, 116
+	JNA	invalid_voltage
+
+	cmp voltage, 137
+	JA test_220
+	jmp valid_voltage
+
+	test_220:
+		cmp voltage, 209
+		JNA invalid_voltage
+
+		cmp voltage, 230
+		JA invalid_voltage
+		JMP valid_voltage
+
+	invalid_voltage:
+		lea bx, msg_error_invalid_voltage
+		call printf_s
+
+		mov ah, 4ch
+		mov al, 1
+		int 21h
+	
+	valid_voltage:
+		ret
+
+	check_voltage ENDP
+	
 .STARTUP
 
-	XOR AX, 	AX
-	XOR BX, 	BX
+	;-----------------------------getting input
+	lea bx, in_buffer
+	call get_argv
 
-	mov flag_i, 0
-	mov flag_o, 0
-	mov flag_v, 0
-	mov exit_code, 0
-
-	call get_argv_pointer
-	call get_file_name
-	call get_output_file_name
 	call get_tension
+	call get_output_file_name
+	call get_file_name
+	;------------------------------converting input
 
-	lea dx, tension_atoi
-	call printf
+	lea bx, voltage_atoi
+	call atoi
+	mov voltage, ax
+	
+	;-----------------------------checking input
 
 	lea dx, input_file_name
-	call printf
+	call open_file
 
-	lea dx, output_file_name
-	call printf
-		
+	call check_voltage
+
+	;-----------------------------processing
+
+	lea bx, voltage_atoi
+	call printf_s
+
+	lea bx, output_file_name
+	call printf_s
+
+	lea bx, input_file_name
+	call printf_s
+
+
+
 .EXIT 0
 
 END
